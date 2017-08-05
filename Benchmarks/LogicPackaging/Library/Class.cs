8c9557706f4e67a942fa90f5ddcac1a6f493f09e
Łukasz Mrozek <morskk@gmail.com>
2017-08-05 15:33:56 +0200

    Added case for using logic packaged in static methods, with which data is communicated in classes

---
+++ Benchmarks/LogicPackaging/Library/Class.cs
@@ -0,0 +1,18 @@
+﻿namespace DotNetPerf.Benchmarks.LogicPackaging.Library
+{
+    public sealed class Class<T>
+    {
+        public T Start { get; }
+        public bool HasOpenStart { get; }
+        public T End { get; }
+        public bool HasOpenEnd { get; }
+
+        public Class(T start, bool hasOpenStart, T end, bool hasOpenEnd)
+        {
+            Start = start;
+            HasOpenStart = hasOpenStart;
+            End = end;
+            HasOpenEnd = hasOpenEnd;
+        }
+    }
+}
--- Benchmarks/LogicPackaging/LogicPackagingBenchmark.cs
+++ Benchmarks/LogicPackaging/LogicPackagingBenchmark.cs
@@ -10,25 +10,45 @@ namespace DotNetPerf.Benchmarks.LogicPackaging
     [Config(typeof(Config))]
     public class LogicPackagingBenchmark
     {
-        private ConsumerStruct<DateTime> _left;
-        private ConsumerStruct<DateTime> _right;
+        private ConsumerStruct<DateTime> _leftStruct;
+        private ConsumerStruct<DateTime> _rightStruct;
+        private ConsumerStructWithStruct<DateTime> _leftStructWithStruct;
+        private ConsumerStructWithStruct<DateTime> _rightStructWithStruct;
+        private ConsumerStructWithClass<DateTime> _leftStructWithClass;
+        private ConsumerStructWithClass<DateTime> _rightStructWithClass;
 
         public void Setup()
         {
-            _left = new ConsumerStruct<DateTime>(new DateTime(2000, 1, 10), false, new DateTime(2000, 1, 20), true);
-            _right = new ConsumerStruct<DateTime>(new DateTime(2000, 1, 15), false, new DateTime(2000, 1, 25), false);
+            _leftStruct = new ConsumerStruct<DateTime>(new DateTime(2000, 1, 10), false, new DateTime(2000, 1, 20), true);
+            _rightStruct = new ConsumerStruct<DateTime>(new DateTime(2000, 1, 15), false, new DateTime(2000, 1, 25), false);
+            _leftStructWithStruct = new ConsumerStructWithStruct<DateTime>(new DateTime(2000, 1, 10), false, new DateTime(2000, 1, 20), true);
+            _rightStructWithStruct = new ConsumerStructWithStruct<DateTime>(new DateTime(2000, 1, 15), false, new DateTime(2000, 1, 25), false);
+            _leftStructWithClass = new ConsumerStructWithClass<DateTime>(new DateTime(2000, 1, 10), false, new DateTime(2000, 1, 20), true);
+            _rightStructWithClass = new ConsumerStructWithClass<DateTime>(new DateTime(2000, 1, 15), false, new DateTime(2000, 1, 25), false);
         }
         
         [Benchmark]
         public ConsumerStruct<DateTime> Static_method_using_parameters_consumed_by_ConsumerStruct()
         {
-            return _left.IntersectUsingStaticMethodWithParameters(_right);
+            return _leftStruct.IntersectUsingStaticMethodWithParameters(_rightStruct);
         }
         
         [Benchmark]
         public ConsumerStruct<DateTime> Static_method_using_structures_consumed_by_ConsumerStruct()
         {
-            return _left.IntersectUsingStaticMethodWithStructures(_right);
+            return _leftStruct.IntersectUsingStaticMethodWithStructures(_rightStruct);
+        }
+        
+        [Benchmark]
+        public ConsumerStructWithStruct<DateTime> Static_method_using_structures_consumed_by_ConsumerWithStruct()
+        {
+            return _leftStructWithStruct.IntersectUsingStaticMethodWithStructures(_rightStructWithStruct);
+        }
+        
+        [Benchmark]
+        public ConsumerStructWithClass<DateTime> Static_method_using_structures_consumed_by_ConsumerWithClass()
+        {
+            return _leftStructWithClass.IntersectUsingStaticMethodWithStructures(_rightStructWithClass);
         }
         
         public sealed class Config : ManualConfig
