abc058c6d2d776f2c8287509ef69334b46af4bc1
Łukasz Mrozek <morskk@gmail.com>
2017-08-19 11:04:07 +0200

    Added common base config

---
+++ Benchmarks/LogicPackaging/BaseConfig.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Jobs;
+using DotNetPerf.Infrastructure.Columns;
+
+namespace DotNetPerf.Benchmarks.LogicPackaging
+{
+    public class BaseConfig : ManualConfig
+    {
+        public BaseConfig()
+        {
+            Add(new MemoryDiagnoser());
+            //Add(RPlotExporter.Default);
+            //Add(CsvMeasurementsExporter.Default);
+            Add(Job.LegacyJitX86);
+            Add(Job.LegacyJitX64);
+            Add(Job.RyuJitX64);
+        }
+    }
+}
