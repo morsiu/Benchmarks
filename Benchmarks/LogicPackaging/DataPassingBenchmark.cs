e89513dd3cae6ac2f2cd2fdd77cd4675225375ac
Łukasz Mrozek <morskk@gmail.com>
2017-08-19 11:05:51 +0200

    Added benchmark for measuring cost of passing data around

---
+++ Benchmarks/LogicPackaging/DataPassingBenchmark.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using DotNetPerf.Benchmarks.LogicPackaging.Consumer;
+
+namespace DotNetPerf.Benchmarks.LogicPackaging
+{
+    [Config(typeof(BaseConfig))]
+    public class DataPassingBenchmark
+    {
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private static ConsumerStructureWithInlineData<DateTime> LogicInline(
+            ConsumerStructureWithInlineData<DateTime> a,
+            ConsumerStructureWithInlineData<DateTime> b,
+            int depth)
+        {
+            return depth == 0 ? a.IntersectUsingStaticMethodWithStructures(b) : LogicInline(a, b, depth - 1);
+        }
+        
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private static ConsumerStructureWithInlineData<DateTime> LogicStructure(
+            ConsumerStructureWithInlineData<DateTime> a,
+            ConsumerStructureWithInlineData<DateTime> b,
+            int depth)
+        {
+            return depth == 0 ? a.IntersectUsingStaticMethodWithStructures(b) : LogicStructure(a, b, depth - 1);
+        }
+        
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private static ConsumerStructureWithInlineData<DateTime> LogicClass(
+            ConsumerStructureWithInlineData<DateTime> a,
+            ConsumerStructureWithInlineData<DateTime> b,
+            int depth)
+        {
+            return depth == 0 ? a.IntersectUsingStaticMethodWithStructures(b) : LogicClass(a, b, depth - 1);
+        }
+
+        [Params(100)]
+        public int Depth;
+
+        [Benchmark]
+        public ConsumerStructureWithInlineData<DateTime> Inline()
+        {
+            return LogicInline(
+                new ConsumerStructureWithInlineData<DateTime>(new DateTime(2000, 1, 1), true, new DateTime(2000, 1, 10), false),
+                new ConsumerStructureWithInlineData<DateTime>(new DateTime(1999, 12, 25), false, new DateTime(2000, 1, 5), true),
+                Depth);
+        }
+        
+        [Benchmark]
+        public ConsumerStructureWithInlineData<DateTime> Class()
+        {
+            return LogicClass(
+                new ConsumerStructureWithInlineData<DateTime>(new DateTime(2000, 1, 1), true, new DateTime(2000, 1, 10), false),
+                new ConsumerStructureWithInlineData<DateTime>(new DateTime(1999, 12, 25), false, new DateTime(2000, 1, 5), true),
+                Depth);
+        }
+        
+        [Benchmark]
+        public ConsumerStructureWithInlineData<DateTime> Structu()
+        {
+            return LogicStructure(
+                new ConsumerStructureWithInlineData<DateTime>(new DateTime(2000, 1, 1), true, new DateTime(2000, 1, 10), false),
+                new ConsumerStructureWithInlineData<DateTime>(new DateTime(1999, 12, 25), false, new DateTime(2000, 1, 5), true),
+                Depth);
+        }
+    }
+}
