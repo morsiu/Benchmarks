0f5067e0ce2f978b5d84d4763943c14855a1122d
Łukasz Mrozek <morskk@gmail.com>
2017-08-20 18:45:00 +0200

    Added missing file

---
+++ LibraryInterfacePerformance/IRange.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LibraryInterfacePerformance
+{
+    public interface IRange<in T, TRange>
+        where T : IComparable<T>
+        where TRange : IRange<T, TRange>
+    {
+        TRange Intersect(TRange other);
+
+        bool IntersectsWith(TRange other);
+    }
+}
