06f74834e23e753dc92fe10c2d09063150ead567
Łukasz Mrozek <morskk@gmail.com>
2017-08-21 17:42:36 +0200

    Avoid problem with string renaming by using constants

--- LibraryInterfacePerformance/BenchmarkOfAllApproaches.cs
+++ LibraryInterfacePerformance/BenchmarkOfAllApproaches.cs
@@ -7,9 +7,16 @@ namespace LibraryInterfacePerformance
     [Config(typeof(BenchmarkConfig))]
     public class BenchmarkOfAllApproaches
     {
+        private const string GenericsAndInterfacesRef = "GenericsAndInterfacesRef";
+        private const string GenericsAndInterfacesRefAndReturn = "GenericsAndInterfacesRefAndReturn";
+        private const string IntermediatePlainValues = "IntermediatePlainValues";
+        private const string AggregatedStructure = "AggregatedStructure";
+        private const string IntermediateClass = "IntermediateClass";
+        private const string IntermediateStructure = "IntermediateStructure";
+
         private IAllBenchmarks _allBenchmarks;
         
-        [Params("GenericsAndInterfacesRef", "GenericsAndInterfacesRefAndReturn", "IntermediatePlainValues", "AggregatedStructure", "IntermediateClass", "IntermediateStructure")]
+        [Params(GenericsAndInterfacesRef, GenericsAndInterfacesRefAndReturn, IntermediatePlainValues, AggregatedStructure, IntermediateClass, IntermediateStructure)]
         public string ApproachName { get; set; }
 
         public const int Count = 1_000_000;
@@ -36,22 +43,22 @@ namespace LibraryInterfacePerformance
         {
             switch (approachName)
             {
-                case "GenericsAndInterfacesRef":
+                case GenericsAndInterfacesRef:
                     return new BenchmarkOfAnApproach<Range<int>, Ranges<int>>(
                         rangeCount, new Ranges<int>());
-                case "GenericsAndInterfacesRefAndReturn":
+                case GenericsAndInterfacesRefAndReturn:
                     return new BenchmarkOfAnApproach<GenericsAndInterfacesRefAndReturn.Consumer.Range<int>, GenericsAndInterfacesRefAndReturn.Consumer.Ranges<int>>(
                         rangeCount, new GenericsAndInterfacesRefAndReturn.Consumer.Ranges<int>());
-                case "IntermediatePlainValues":
+                case IntermediatePlainValues:
                     return new BenchmarkOfAnApproach<IntermediatePlainValues.Consumer.Range<int>, IntermediatePlainValues.Consumer.Ranges<int>>(
                         rangeCount, new IntermediatePlainValues.Consumer.Ranges<int>());
-                case "AggregatedStructure":
+                case AggregatedStructure:
                     return new BenchmarkOfAnApproach<AggregatedStructure.Consumer.Range<int>, AggregatedStructure.Consumer.Ranges<int>>(
                         rangeCount, new AggregatedStructure.Consumer.Ranges<int>());
-                case "IntermediateClass":
+                case IntermediateClass:
                     return new BenchmarkOfAnApproach<IntermediateClass.Consumer.Range<int>, IntermediateClass.Consumer.Ranges<int>>(
                         rangeCount, new IntermediateClass.Consumer.Ranges<int>());
-                case "IntermediateStructure": 
+                case IntermediateStructure: 
                     return new BenchmarkOfAnApproach<IntermediateStructure.Consumer.Range<int>, IntermediateStructure.Consumer.Ranges<int>>(
                         rangeCount, new IntermediateStructure.Consumer.Ranges<int>());
                 default : throw new ArgumentOutOfRangeException(nameof(approachName), approachName, "Uknown approach");
