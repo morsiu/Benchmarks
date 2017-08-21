c5ee6c40fc0c48fc13e57623979920ac2d6d14c4
Łukasz Mrozek <morskk@gmail.com>
2017-08-21 22:28:25 +0200

    Renamed 'Aggregated structure' approach, that does not use ref in library interface

---
+++ LibraryInterfacePerformance/AggregatedStructure/Consumer/Range.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Impl = LibraryInterfacePerformance.AggregatedStructure.Library;
+
+namespace LibraryInterfacePerformance.AggregatedStructure.Consumer
+{
+    public struct Range<T> : IRange<T, Range<T>>
+        where T : IComparable<T>
+    {
+        private readonly Impl.Range<T> _implementation;
+
+        public Range(T start, bool openStart, T end, bool openEnd)
+        {
+            _implementation = new Impl.Range<T>(start, openStart, end, openEnd);
+        }
+
+        private Range(Impl.Range<T> implementation)
+        {
+            _implementation = implementation;
+        }
+
+        public bool Empty => _implementation.Empty;
+        public bool OpenStart => _implementation.OpenStart;
+        public bool OpenEnd => _implementation.OpenEnd;
+
+        public bool IntersectsWith(Range<T> other) =>
+            _implementation.IntersectsWith(other._implementation);
+
+        public Range<T> Intersect(Range<T> other) =>
+            new Range<T>(_implementation.Intersect(other._implementation));
+    }
+}
