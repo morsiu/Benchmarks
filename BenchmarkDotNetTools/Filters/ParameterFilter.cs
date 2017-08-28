ef08215031b131365230d9697223d936a6b91187
Łukasz Mrozek <morskk@gmail.com>
2017-08-28 20:08:27 +0200

    Added filter of benchmarks by their parameter's value

---
+++ BenchmarkDotNetTools/Filters/ParameterFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using BenchmarkDotNet.Filters;
+using BenchmarkDotNet.Running;
+
+namespace BenchmarkDotNetTools.Filters
+{
+    public sealed class ParameterFilter : IFilter
+    {
+        private readonly string _nameOfParameter;
+        private readonly Predicate<object> _predicateOnParameterValue;
+
+        public ParameterFilter(string nameOfParameter, Predicate<object> predicateOnParameterValue)
+        {
+            _nameOfParameter = nameOfParameter;
+            _predicateOnParameterValue = predicateOnParameterValue;
+        }
+        
+        public bool Predicate(Benchmark benchmark)
+        {
+            return benchmark.Parameters.Items.Any(x => Equals(x.Name, _nameOfParameter) && _predicateOnParameterValue(x.Value));
+       }
+    }
+}
