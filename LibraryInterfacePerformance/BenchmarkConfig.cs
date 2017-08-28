c64edfee9c539279841aedcbf987c7d79a470ea3
Łukasz Mrozek <morskk@gmail.com>
2017-08-28 19:51:17 +0200

    Removed units in cells from CSV results

--- LibraryInterfacePerformance/BenchmarkConfig.cs
+++ LibraryInterfacePerformance/BenchmarkConfig.cs
@@ -1,8 +1,11 @@
-﻿using BenchmarkDotNet.Configs;
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Exporters;
 using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Horology;
 using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Reports;
 
 namespace LibraryInterfacePerformance
 {
@@ -15,7 +18,14 @@ namespace LibraryInterfacePerformance
             Add(Job.LegacyJitX64);
             Add(Job.LegacyJitX86);
             Add(RPlotExporter.Default);
-            Add(CsvMeasurementsExporter.Default);
+            Add(new CsvMeasurementsExporter(CsvSeparator.CurrentCulture,
+                new SummaryStyle
+                {
+                    PrintUnitsInContent = false,
+                    PrintUnitsInHeader = true,
+                    TimeUnit = TimeUnit.Nanosecond,
+                    SizeUnit = SizeUnit.B
+                }));
         }
     }
 }
